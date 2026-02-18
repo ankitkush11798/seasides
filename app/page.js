30ada337703e886c5daca36d0f65e51c1cdd6f2c
ankit1x01 <ankit1x01@gmail.com>
2026-02-18 18:46:12 +0530

    feat: Implement initial Seasides 2026 homepage with dynamic components and add a strategic networking plan.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
-import CTFPopup from '@/components/shared/CTFPopup';
 import dynamic from 'next/dynamic';
 
 // Critical components loaded immediately for LCP
@@ -86,7 +85,7 @@ export default function Home() {
         <ReachUs />
       </div>
       <Footer />
-      <CTFPopup />
+      {/* <CTFPopup /> */}
     </main>
   );
 }
