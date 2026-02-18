0cd9bb1ae6abdae9c4d626ef63ab7af9d2eac459
ankit1x01 <ankit1x01@gmail.com>
2026-02-18 19:05:30 +0530

    feat: Enhance CTFPopup component with improved visibility and animations, and integrate it into the homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
+import CTFPopup from '@/components/shared/CTFPopup';
 import dynamic from 'next/dynamic';
 
 // Critical components loaded immediately for LCP
@@ -85,7 +86,7 @@ export default function Home() {
         <ReachUs />
       </div>
       <Footer />
-      {/* <CTFPopup /> */}
+      <CTFPopup />
     </main>
   );
 }
